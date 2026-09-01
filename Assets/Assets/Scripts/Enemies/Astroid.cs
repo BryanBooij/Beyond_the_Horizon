b62f8be69d62f998169961fd75cce698b8591313
Bryan_1025561 <1025561@hr.nl>
2026-09-01 19:21:49 +0200

    added an astroid that moves from right to left

---
+++ Assets/Assets/Scripts/Enemies/Astroid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * (moveSpeed * Time.deltaTime));
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
