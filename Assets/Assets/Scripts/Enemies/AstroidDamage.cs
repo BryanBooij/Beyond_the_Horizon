5e23c7b1b8abdd546a0f1caabd6b330f0237d408
efeg312 <1081187@hr.nl>
2026-09-03 14:09:40 +0200

    player has healthbar now and asteroids deal damage

---
+++ Assets/Assets/Scripts/Enemies/AstroidDamage.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies
+{
+    public class AstroidDamage: MonoBehaviour
+    {
+        public int damage;
+        public PlayerHealth playerHealth;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                playerHealth.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        public Slider healthBarSlider;
+        public TextMeshProUGUI healthBarValueText;
+
+        public int maxHealth = 100;
+        public int currentHealth;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        private void Update()
+        {
+            healthBarValueText.text = currentHealth + "/" + maxHealth;
+            healthBarSlider.value = currentHealth;
+            healthBarSlider.maxValue = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            if (currentHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
