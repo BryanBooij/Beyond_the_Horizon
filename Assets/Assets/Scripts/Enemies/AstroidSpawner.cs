5cbdbadb80992a17b3911c27bb12a621769a1baf
Bryan_1025561 <1025561@hr.nl>
2026-09-10 11:58:05 +0200

    fixed canvas height and width to be transparent to the users screen

--- Assets/Assets/Scripts/Enemies/AstroidSpawner.cs
+++ Assets/Assets/Scripts/Enemies/AstroidSpawner.cs
@@ -2,24 +2,42 @@ using UnityEngine;
 
 public class AstroidSpawner : MonoBehaviour
 {
-    public GameObject[] asteroidPrefabs; // list to add as many astroids as preferred
+    public GameObject[] asteroidPrefabs;
     public float spawnInterval = 1f;
-    public float minY = -4.1f;
-    public float maxY = 4.1f;
+
+    private float minY;
+    private float maxY;
 
     void Start()
     {
-        InvokeRepeating(nameof(SpawnAsteroid), 0f, spawnInterval); // every second repeat SpawnAstroid to spawn a astroid
+        InvokeRepeating(nameof(SpawnAsteroid), 0f, spawnInterval);
     }
 
     void SpawnAsteroid()
     {
-        if (asteroidPrefabs == null || asteroidPrefabs.Length == 0) return; // check to see if a minimum of 1 proper prefabs is loaded
+        if (asteroidPrefabs == null || asteroidPrefabs.Length == 0)
+        {
+            return;
+        }
+        GameObject prefabToSpawn = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
+        // keep astroids spawning in frame
+        Camera cam = Camera.main;
+        float cameraHeight = cam.orthographicSize;
+        Collider2D asteroidCollider = prefabToSpawn.GetComponent<Collider2D>();
+        float halfAsteroidHeight = 0f;
+        // keeping astroids perfectly in frame
+        if (asteroidCollider != null)
+        {
+            halfAsteroidHeight = asteroidCollider.bounds.extents.y;
+        }
+        
+        minY = cam.transform.position.y - cameraHeight + halfAsteroidHeight;
+        maxY = cam.transform.position.y + cameraHeight - halfAsteroidHeight;
+        
+        float randomY = Random.Range(minY, maxY);
 
-        float randomY = Random.Range(minY, maxY); // randomize spawn Vertical location 
-        Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0f); // spawn astroid on previous randomized location
+        Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0f);
 
-        GameObject prefabToSpawn = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)]; // randomize which astroid from the list is spawned
         Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
     }
 }
