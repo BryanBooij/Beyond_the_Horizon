c5b73c0128d342277d188fa5830a51b755f32800
Bryan_1025561 <1025561@hr.nl>
2026-09-17 14:53:07 +0200

    added template level 1

---
+++ Assets/Assets/Scripts/Enemies/AstroidLevelSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class AstroidLevelSpawner : MonoBehaviour
+{
+    public GameObject[] asteroidPrefabs;
+    public float spawnInterval = 1f;
+    public float maxSpawns = 10;
+    public float currentSpawns = 0;
+
+    private float minY;
+    private float maxY;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnAsteroid), 0f, spawnInterval);
+    }
+
+    void SpawnAsteroid()
+    {
+        if (currentSpawns >= maxSpawns)
+        {
+            CancelInvoke(nameof(SpawnAsteroid));
+            return;
+        }
+        
+        if (asteroidPrefabs == null || asteroidPrefabs.Length == 0)
+        {
+            return;
+        }
+        GameObject prefabToSpawn = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
+        // keep astroids spawning in frame
+        Camera cam = Camera.main;
+        float cameraHeight = cam.orthographicSize;
+        Collider2D asteroidCollider = prefabToSpawn.GetComponent<Collider2D>();
+        float halfAsteroidHeight = 0f;
+        // keeping astroids perfectly in frame
+        if (asteroidCollider != null)
+        {
+            halfAsteroidHeight = asteroidCollider.bounds.extents.y;
+        }
+        
+        minY = cam.transform.position.y - cameraHeight + halfAsteroidHeight;
+        maxY = cam.transform.position.y + cameraHeight - halfAsteroidHeight;
+        
+        float randomY = Random.Range(minY, maxY);
+
+        Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0f);
+
+        Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
+        currentSpawns++;
+    }
+}
--- Assets/Assets/Scripts/Game/EnemySpaceshipLevelSpawner.cs
+++ Assets/Assets/Scripts/Game/EnemySpaceshipLevelSpawner.cs
@@ -1,13 +1,17 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Enemyspaceshipspawner : MonoBehaviour
+public class EnemySpaceshipLevelSpawner : MonoBehaviour
 {
     private float minY;
     private float maxY;
 
     public GameObject Enemyspaceship;
+    public GameObject EnemyBoss;
     public float spawnInterval = 15f;
     public float startDelay = 5f;
+    public int spawnAmount = 10;
+
+    private int spawnedAmount = 0;
 
     void Start()
     {
@@ -31,9 +35,25 @@ public class Enemyspaceshipspawner : MonoBehaviour
 
     void SpawnEnemy()
     {
+        if (spawnedAmount >= spawnAmount)
+        {
+            CancelInvoke(nameof(SpawnEnemy));
+            SpawnBoss();
+            return;
+        }
+
         float randomY = Random.Range(minY, maxY);
         Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0f);
 
         Instantiate(Enemyspaceship, spawnPos, Quaternion.identity);
+
+        spawnedAmount++;
+    }
+
+    void SpawnBoss()
+    {
+        float randomY = Random.Range(minY, maxY);
+        Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0f);
+        Instantiate(EnemyBoss, spawnPos, Quaternion.identity);
     }
 }
